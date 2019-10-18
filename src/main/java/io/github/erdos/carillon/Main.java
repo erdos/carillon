258c9502ccded7fef0c063d4cdf5ed61f0b4f301
Janos Erdos <janos.erdos@transferwise.com>
2019-10-18 21:30:34 +0200

    working on cli

--- src/main/java/io/github/erdos/carillon/Main.java
+++ src/main/java/io/github/erdos/carillon/Main.java
@@ -5,6 +5,9 @@ import io.github.erdos.carillon.eval.RT;
 import io.github.erdos.carillon.objects.Expression;
 import io.github.erdos.carillon.reader.Reader;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PushbackReader;
@@ -12,33 +15,53 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
+		Arrays.stream(args).filter(x -> !x.startsWith("-")).forEach(fname -> {
+			try {
+				runFile(new File(fname));
+			} catch (FileNotFoundException e) {
+				System.err.println(e.getMessage());
+				System.exit(1);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		});
 
-        // TODO: iterate over files to eval them.
+		if (Arrays.stream(args).anyMatch(x -> x.equals("--repl"))) {
+			repl();
+		}
+	}
 
-        if (Arrays.stream(args).anyMatch(x->x.equals("--repl"))) {
-            repl();
-        }
+	private static void runFile(File file) throws IOException {
+		try (FileReader fr = new FileReader(file);
+		     PushbackReader reader = new PushbackReader(fr)) {
+			while (true) {
+				Expression expression = Reader.read(reader);
 
-        repl();
-    }
+				if (expression == null) return;
 
-    private static void repl() throws IOException {
-        System.out.println("BEL REPL. Press ^D to exit.");
-        PushbackReader reader = new PushbackReader(new InputStreamReader(System.in));
-        while (true) {
-            System.out.print("> ");
-            System.out.flush();
-            Expression expression = Reader.read(reader);
+				Expression result = RT.eval(expression);
+			}
+		}
+	}
 
-            try {
-                Expression result = RT.eval(expression);
-                System.out.println(result.toString());
+	private static void repl() throws IOException {
+		System.out.println("BEL REPL. Press ^D to exit.");
+		PushbackReader reader = new PushbackReader(new InputStreamReader(System.in));
+		while (true) {
+			System.out.print("> ");
+			System.out.flush();
+			Expression expression = Reader.read(reader);
 
-            } catch (EvaluationException e) {
-                System.err.println("ERROR.");
-                System.err.println(e.getMessage());
-            }
-        }
-    }
+			try {
+				Expression result = RT.eval(expression);
+				System.out.println(result.toString());
+
+			} catch (EvaluationException e) {
+				System.err.println("ERROR.");
+				System.err.println(e.getMessage());
+			}
+		}
+	}
 }
